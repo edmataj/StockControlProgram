04599ee9dd29d181d89489c72146bb72e2e69c0b
edmataj <37461940+edmataj@users.noreply.github.com>
2021-02-02 10:06:18 +0000

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,58 @@
+public class Product 
+{
+	//the attributes
+	private String name;
+	private int stockLevel;
+	private double price;
+	
+	//the constructor
+	public Product (String nameIn, int StockLevelIn, double priceIn)
+	{
+		name= nameIn;
+		stockLevel= StockLevelIn;
+		price= priceIn;
+	}
+	//the methods
+	
+	//method to restock
+	public int reStock(int amountIn)
+	{
+		stockLevel= stockLevel + amountIn;
+		return stockLevel;
+	}
+	
+	//method to sell
+	public double sell (int quantityIn)
+	{
+	
+		stockLevel= stockLevel - quantityIn;
+			
+		
+		return quantityIn * price;
+			
+	}
+	
+	//method to set a new price
+	public void setPrice (double priceIn)
+	{
+		price= priceIn;
+	}
+	
+	//method to get the prduct name
+	public String getName()
+	{
+		return name;
+	}
+	
+	//method to get the prduct stock level
+	public int getStockLevel()
+	{
+		return stockLevel;
+	}
+	
+	//method to get the prduct price
+	public double getPrice()
+	{
+		return price;
+	}
+}
